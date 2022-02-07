7c88983dc5104bb4b4d658d41f0cf64fe56fda67
alline.cardoso <migowskialline@gmail.com>
2022-02-06 21:29:03 -0300

    subscribe método list

--- src/app/viewa/list-products/list-products.component.ts
+++ src/app/viewa/list-products/list-products.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from '../checkout/checkout.service';
+import { Products } from './products.model';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.css']
+  styleUrls: ['./list-products.component.css'],
 })
 export class ListProductsComponent implements OnInit {
-
-  constructor() { }
+  listProducts: Products[] = [];
+  constructor(private checkoutService: CheckoutService) {}
 
   ngOnInit(): void {
+    this.checkoutService.getListProducts().subscribe((products) => {
+      this.listProducts = products;
+    });
   }
-
 }
