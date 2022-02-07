d0b6de4a672d7f60b47f67f41666fc63b1502b48
alline.cardoso <migowskialline@gmail.com>
2022-02-06 21:29:34 -0300

    inclusão atributo descricao

--- src/app/viewa/list-products/products.model.ts
+++ src/app/viewa/list-products/products.model.ts
@@ -4,18 +4,21 @@ export class Products {
   'category': string;
   'img': string;
   'price': number;
+  'descricao': string;
 
   constructor(
     id: number,
     name: string,
     category: string,
     img: string,
-    price: number
+    price: number,
+    descricao: string
   ) {
     this.id = id;
     this.name = name;
     this.category = category;
     this.img = img;
     this.price = price;
+    this.descricao = descricao;
   }
 }
