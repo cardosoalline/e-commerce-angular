18fd6eb1d078016c523c659adb2dec809a6f7bc0
alline.cardoso <migowskialline@gmail.com>
2022-02-06 21:22:46 -0300

    adição da rota de lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './viewa/home/home.component';
+import { ListProductsComponent } from './viewa/list-products/list-products.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'list-products', component: ListProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
