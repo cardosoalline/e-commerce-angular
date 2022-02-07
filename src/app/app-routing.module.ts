b0a809e03bf356e561385af6a9f9fd7c4e3c1ce8
alline.cardoso <migowskialline@gmail.com>
2022-02-07 09:11:53 -0300

    inclusão rota componente checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './viewa/checkout/checkout.component';
 import { HomeComponent } from './viewa/home/home.component';
 import { ListProductsComponent } from './viewa/list-products/list-products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list-products', component: ListProductsComponent },
+  { path: 'checkout', component: CheckoutComponent },
 ];
 
 @NgModule({
