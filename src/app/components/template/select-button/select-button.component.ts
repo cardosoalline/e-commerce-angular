8ff6a7129f2faff3c211a5ca1d3be5a1e0c6fe7b
alline.cardoso <migowskialline@gmail.com>
2022-02-07 08:24:06 -0300

    correta inclusão de métodos sleção e exclusão

--- src/app/components/template/select-button/select-button.component.ts
+++ src/app/components/template/select-button/select-button.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from 'src/app/viewa/checkout/checkout.service';
 
 @Component({
   selector: 'app-select-button',
   templateUrl: './select-button.component.html',
-  styleUrls: ['./select-button.component.css']
+  styleUrls: ['./select-button.component.css'],
 })
 export class SelectButtonComponent implements OnInit {
+  public disabled: boolean = false;
+  constructor(private checkoutService: CheckoutService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  selectProduct() {
+    this.disabled = true;
+    this.checkoutService.selectProduct();
   }
 
+  unselectProduct() {
+    this.disabled = false;
+    this.checkoutService.unselectProduct();
+  }
 }
