4243fb1b0c51f791c5e366ac05cca43bf0b21381
alline.cardoso <migowskialline@gmail.com>
2022-02-08 17:28:52 -0300

    adição botão carrinho vazio e ngModel

--- src/app/viewa/checkout/checkout.component.ts
+++ src/app/viewa/checkout/checkout.component.ts
@@ -15,12 +15,21 @@ export class CheckoutComponent implements OnInit {
   hide: boolean = true;
   form: any;
 
+  //variável iniciando com um objeto vazio para usar two-way binding
+  client: any = {};
+
   constructor(
     private checkoutService: CheckoutService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
+    //buscando o id form e colocando dentro da variável form
+    this.form = document.querySelector('#form');
+    //tratando o evento click para evitar o comportamento padão do form
+    this.form.addEventListener('click', (event: any) => {
+      event.preventDefault();
+    });
     this.totalPrice = this.checkoutService.totalPrice;
     this.listSelectedProducts = this.checkoutService.listSelectProducts;
     this.toggleButton();
@@ -54,7 +63,25 @@ export class CheckoutComponent implements OnInit {
   }
 
   payment(): void {
-    this.checkoutService.showMessage('Payment', true);
-    this.router.navigate(['../list-products']);
+    if (
+      this.client.name === undefined ||
+      this.client.endereco === undefined ||
+      this.client.password === undefined
+    ) {
+      this.checkoutService.showMessage(
+        'Por favor preencha com dados válidos!',
+        false
+      );
+    } else {
+      //uso de template litearal com crase para poder interpolar variáveis na string da msg
+      this.checkoutService.showMessage(
+        `Pagamento realizado com sucesso! Pedido confirmado e será entregue em ${this.client.endereco}.\n ${this.client.name},obrigada por escolher True Love! :D`,
+        true
+      );
+      this.checkoutService.totalPrice = 0;
+      this.checkoutService.listSelectProducts = [];
+      this.router.navigate(['../list-products']);
+    }
+    //this.checkoutService.showMessage('Payment', true);
   }
 }
